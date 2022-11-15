fb9c2c95d5f3c3b39c2725df34c2e68ae3fbaa3b
Falderian <nikitastaselovich@gmail.com>
2022-11-15 13:00:39 +0300

    finish: finish project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.scss';
 import { HomePage } from './homepage/HomePage';
-import { Sidebar } from './layout/sidebar/Sidebar';
+import { Sidebar } from './sidebar/Sidebar';
 import { UserPage } from './userpage/UserPage';
 
 function App() {
